dd81ce39793f40d608e6e1b12b4fb99675407a10
Jake Bladt <jake@jakebladt.com>
2016-10-23 21:30:45 -0400

    +Pillow

--- provision.sh
+++ provision.sh
@@ -5,5 +5,9 @@ apt-get update
 # install python tools
 apt-get install python3-setuptools
 easy_install3 pip
+
+apt-get install python3-dev python3-setuptools libtiff4-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev
+
 pip3.4 install pylint
 pip3.4 install pyflakes
+pip3 install Pillow
